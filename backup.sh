06990db476234b2a5aec02007ba7cba0d85943dd
yz-wulala <yzjiang18@fudan.edu.cn>
2020-06-08 13:37:34 +0800

    backup

--- backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ./project
-git add -A
-git commit -m "backup"
-ssh-add ../github
-git remote set-url origin git+ssh://git@github.com/yz-wulala/iServer01Bkup.git
-git push -u origin master
